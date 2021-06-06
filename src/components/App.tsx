35ae8f4b8de0691d73d76554db33ccef236702b8
NimiBahar <nimrodbahar@gmail.com>
2021-06-06 09:46:21 +0300

    react redux intro project

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../state";
+import RepositoriesList from "../components/RepositoriesList";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search for a package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { serachRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    serachRepositories(term);
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={onChange} />
+        <button>Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
+    </div>
+  );
+};
+
+export default RepositoriesList;
